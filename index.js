a1688d2a38f572a430b3419924c170adfd8e4b39
alex11920 <av61799@gmail.com>
2024-04-29 07:30:18 +0600

    read data done

--- index.js
+++ index.js
@@ -27,16 +27,32 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const craftCollection = client.db("craftDB").collection("craft");
+
+    // Create
+    app.post("/craft", async (req, res) => {
+      const newCraft = req.body;
+      console.log(newCraft);
+      const result = await craftCollection.insertOne(newCraft);
+      res.send(result);
+    });
+
+    // Read
+    app.get("/craft", async (req, res) => {
+      const cursor = craftCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
