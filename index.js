1fb81f50eb7684764a7ee66590fcffefbf5e35b4
alex11920 <av61799@gmail.com>
2024-04-30 20:05:16 +0600

    finished

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send(`Craftshells server is running`);
+  res.send(`Craftshells server is active`);
 });
 
 // Database String---------------->
